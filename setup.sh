dcb62493120cc8af30cd844c0360a1035701b063
Matthew Coming <coming.matthew@gmail.com>
2026-07-07 02:54:12 -0500

    updated resume

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo apt update && sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+rm -rf aws awscliv2.zip
